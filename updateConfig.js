e87ba6586ae691e6c5c3443031f3d03c990a8502
Conlin Durbin <conlin.durbin@whatnot.com>
2025-08-15 10:53:10 -0400

    Undo config change

--- updateConfig.js
+++ updateConfig.js
@@ -4,8 +4,7 @@ const contents = fs.readFileSync("config.production.json");
 const config = JSON.parse(contents);
 const externalURL = process.env.url || process.env.RENDER_EXTERNAL_URL;
 if (externalURL) {
-	// update the URL in the config
-	config.url = externalURL;
-	config.mail.options.auth.pass = process.env.IMAP_PASSWORD;
-	fs.writeFileSync("config.production.json", JSON.stringify(config, null, 2));
+  // update the URL in the config
+  config.url = externalURL;
+  fs.writeFileSync("config.production.json", JSON.stringify(config, null, 2));
 }
